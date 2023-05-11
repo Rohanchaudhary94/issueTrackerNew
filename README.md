60157e9330fe185bfc73a2d6352950fd66a3b735
rohan raj <write2rohan94@gmail.com>
2023-05-11 17:31:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Issue-Tracker
+node js project
+# Issue-Tracker
+node js project\
+
+# `Home page`\
+![Screenshot from 2023-01-28 11-20-47](https://imgur.com/LWh23nc)
+
+# `Create a new Project`\
+![Screenshot from 2023-01-28 11-34-15](https://imgur.com/R79FEMs)
+
+# `It creating it is added to home page`\
+![Screenshot from 2023-01-28 11-34-35](https://imgur.com/7zoUXrp)
+
+# `opening project `\
+![Screenshot from 2023-01-28 11-36-59](https://imgur.com/rx8PgoN)
+
+# `creating Issue`\
+![Screenshot from 2023-01-28 11-39-24](https://imgur.com/rx8PgoN)
+
+
+# `Hosted link`\
+https://issue-traker-by-rgh.onrender.com/
---
+++ config/mongoose.js
@@ -0,0 +1,21 @@
+// require the library
+const mongoose= require('mongoose');
+//supress wRNING
+mongoose.set('strictQuery', true);
+const path= 'mongodb://localhost/issueTracker';
+//  const path = 'mongodb+srv://shubham21101997:1XKDfXnqsQmSfI8C@codeial.0y4o9t7.mongodb.net/?retryWrites=true&w=majority';
+//connected to database -
+mongoose.connect(path);
+
+//acquire the connection to check if it is successful
+const db=mongoose.connection;
+
+//error
+db.on('error' ,console.error.bind(console,'error connectiong to mongodb'));
+
+//up and running print the message
+db.once('open', function(){
+    console.log('successfully connected to database');
+})
+
+module.exports=db;
